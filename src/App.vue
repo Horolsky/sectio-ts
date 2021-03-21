79b1e0b06821c52278a97df0eaeab9cb27c37e0d
Horolsky <horolsky89@outlook.com>
2021-03-21 19:46:00 +0200

    firebase db init

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,38 @@
 <template>
-
+<p>Sectio Canonis 1.0.0</p>
 </template>
 
 <script>
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 export default {
   name: 'App',
   components: {
     
+  },
+  data: () => ({
+    firebase: null,
+    db: null
+  }),
+  created() {
+    this.initialize();
+  },
+  methods: {
+    initialize() {
+      //FIRESTORE
+      this.firebase = firebase;
+      firebase.initializeApp({
+        apiKey: "AIzaSyBCNrnag3orw2BTIn4hPR2i_n-5eEacr2w",
+        authDomain: "sectio-canonis.firebaseapp.com",
+        databaseURL: "https://sectio-canonis.firebaseio.com",
+        projectId: "sectio-canonis",
+        storageBucket: "sectio-canonis.appspot.com",
+        messagingSenderId: "674839962809",
+        appId: "1:674839962809:web:787b93e13ff8c63c2d6768",
+        measurementId: "G-MRG29FW0RK"
+      });
+      this.db = firebase.firestore();
+    }
   }
 }
 </script>
