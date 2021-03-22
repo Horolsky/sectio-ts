ea1d65f592c801c42746ff035b31dea9dce789f1
Horolsky <horolsky89@outlook.com>
2021-03-22 21:49:13 +0200

    types declaration

---
+++ src/types/common/core.d.ts
@@ -0,0 +1,3 @@
+interface dict {
+    [key: string]: any
+}
---
+++ src/types/core/core.d.ts
@@ -0,0 +1,8 @@
+type Ratio = {
+        num: number,
+        den: number,
+        flt: number,
+        euler: number,
+        primes: number[],
+        powers: number[]
+}
