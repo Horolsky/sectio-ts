51974a8bece197ba5c27bbfa60553542188773a1
Horolsky <horolsky89@outlook.com>
2021-03-22 21:57:02 +0200

    comments on Ratio type

--- src/types/core/core.d.ts
+++ src/types/core/core.d.ts
@@ -1,8 +1,14 @@
 type Ratio = {
-        num: number,
-        den: number,
-        flt: number,
-        euler: number,
-        primes: number[],
-        powers: number[]
-}
+  /** fraction numerator */
+  num: number;
+  /** fraction denominator */
+  den: number;
+  /** float representation */
+  flt: number;
+  /** log2 representation */
+  euler: number;
+  /** prime factors */
+  primes: number[];
+  /** prime factor powers */
+  powers: number[];
+};
