74c65196ecdc2796c3ebe1351bf225757cd5e410
Horolsky <horolsky89@outlook.com>
2021-03-28 01:22:00 +0200

    types upd

--- src/types/common/common.d.ts
+++ src/types/common/common.d.ts
@@ -1,3 +1,7 @@
 interface dict {
     [key: string]: any
 }
+type search_callback = function (any, any):number;
+
+type Readonly<Type> = {readonly [key in keyof Type ]: Type[key]};
+type Optional<Type> = {[key in keyof Type ]?: Type[key]};
--- src/types/core/core.d.ts
+++ src/types/core/core.d.ts
@@ -1,11 +1,5 @@
 
-/** RatioMap jsonized data */
-type rm_data = {
-    ratios: Ratio[],
-    limit: number,
-    range: number,
-    primes: number[]
-};
+
 /** 
  * approximaton prime limit
  * if undefined, will use fast approximation algo
@@ -19,7 +13,7 @@ type plimit = 2 | 3 | 5 | 7 | 11 | 13 | 17;
 type factorisation = {
   [key: string]: number
 }
-
+//type factorisation  = Map<number, number>;
 type ratio = {
   /** fraction numerator */
   readonly num: number;
@@ -29,6 +23,14 @@ type ratio = {
   readonly euler: number;
   /** prime factorisation */
   readonly fact: factorisation
+};
+type interval = number | number[] | ratio | Ratio;
+/** RatioMap jsonized data */
+type rm_data = {
+  ratios: ratio[],
+  limit: number,
+  range: number,
+  primes: number[]
 };
 /**
  * extends Ratio class with temperament parameter
@@ -42,3 +44,56 @@ type RationalApproximation = {
   readonly limit: plimit,
   readonly range: number
 };
+/** canonic model parameters */
+type canon_params = {
+  /** p-limit of the system's rational approximation */
+  limit: null | plimit,
+  /** range of the system's rational approximation */
+  range: number,
+  /** periodic interval of the system */
+  period: null | number | number[],
+  /** temperament representation interval */
+  comma: number
+}
+/** canonic section data */
+type section = {
+  id: number,
+  /** section's full name */
+  name: string,
+  /** section's short code */
+  code: string,
+  /** id of parent section */
+  parent: number,
+  /** ratio to parent section */
+  rtp: number | number[]
+}
+/** canonic raw data schema */
+type canon_schema = {
+  id: number,
+  name: string,
+  code: string,
+  description: string,
+  tags: string[],
+  baseFreq: number,
+  baseStrL: number,
+  baseColor: string,
+  params: canon_params,
+  sections: section[]
+}
+
+type canon_cache = {
+  /** number of sections */
+  size: number,
+  /** adjacency matrix of generator intervals */
+  generators_mt: number[],
+  /** complete adjacency matrix of sections relations */
+  relations_mt: number[],
+  /** parent list (index - section, value - parent's id) */
+  parent_list: number[],
+  /** factorisation primes*/
+  primes: [],
+  /** adjacency matrix of factorisaion vectors */
+  factorisation_mt: number[][],
+  /** full system as ratio in factorisation form */
+  super_ratio_fct: number[]
+}
