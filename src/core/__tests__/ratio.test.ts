b3d1f0aad4283453db1a047e86a9821e52e606a8
Horolsky <horolsky89@outlook.com>
2021-04-05 01:03:10 +0300

    core: ratio tests

---
+++ src/core/__tests__/ratio.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '@jest/globals';
+import { PREC, PRIMES_LOG2 } from '../constants';
+import Ratio from '../ratio';
+
+test("creating Ratio inst from fraction 15:14", () => {
+    const x = new Ratio(15,14);
+    //{2:-1,3:1,5:1,7:-1}
+    const exact = PRIMES_LOG2[3]+PRIMES_LOG2[5]-PRIMES_LOG2[7]-PRIMES_LOG2[2];
+    expect(Math.abs(x.exact_euler - exact) < PREC).toBe(true);
+})
+
+test("Ratio addition: 3/2 + 4/3 = 2/1", () => {
+    const x = new Ratio([3,2] as fraction);
+    const y = new Ratio({2:2,3:-1} as pfv);
+    expect(Math.abs(x.add(y).exact_euler - 1) < PREC).toBe(true);
+})
+
+test("Ratio substraction: 3/2 - 4/3 = 9/8", () => {
+    const x = new Ratio([3,2] as fraction);
+    const y = new Ratio({2:2,3:-1} as pfv);
+    const exact = Math.log2(9)-3;
+    expect(Math.abs(x.sub(y).exact_euler - exact) < PREC).toBe(true);
+})
+
+test("copying Ratio", () => {
+    const x = new Ratio(15,14);
+    expect(x.exact_euler ===  x.copy().exact_euler).toBe(true);
+})
