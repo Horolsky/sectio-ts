e0d7721c804e75435ff473c4fa6d2b7b7392bbdf
Horolsky <horolsky89@outlook.com>
2021-04-04 23:03:48 +0300

    core: canon input handling

--- src/core/canon.ts
+++ src/core/canon.ts
@@ -1,3 +1,6 @@
+import { APPROX_PRIMES } from "./constants";
+import { valid_frac } from "./math";
+import { factorize_int } from "./pfv-methods";
 import RatioMap from "./ratiomap";
 
 export const DEFAULT_SCHEMA: canon_schema = {
@@ -18,12 +21,85 @@ export const DEFAULT_SCHEMA: canon_schema = {
   sections: [],
 };
 
+
+/**
+ * operates on sorted input
+ * returns 1 if correct, else - returns prod of error codes (primes)
+ */
+export const check_sections_validity = (sections: Array<section>):number => {
+  let result_code = 1;
+  sections.forEach((section, i) => {
+    if (section.id === undefined) result_code *= 2;
+    const same_id_i = sections.findIndex(el => el.id == section.id)
+    if (same_id_i >= 0 && same_id_i < i) result_code *= 3;
+    const parent_i = sections.findIndex(el => el.id == section.parent)
+    if (parent_i >= 0 && parent_i >= i) result_code *= 5;
+    if (section.parent === undefined) result_code *= 5;
+    if (typeof section.rtp != "number" && !valid_frac(section.rtp)) result_code *= 7;
+  });
+  return result_code;
+}
+export const check_schema_validity = (schema: canon_schema) => {
+
+  let result_code = check_sections_validity(schema.sections);
+
+  if (
+    typeof schema.params.period == "number" 
+    && schema.params.period < 0
+    ) result_code *= 11;
+  else if (
+    typeof schema.params.period != "number" 
+    && !valid_frac(schema.params.period)
+    ) result_code *= 11;
+  
+  if (typeof schema.params.comma != "number" || schema.params.comma < 0) result_code *= 13;
+  if (typeof schema.params.range != "number" || schema.params.range < 0) result_code *= 17;
+  if (APPROX_PRIMES.indexOf(schema.params.limit as number) < 0 ) result_code *= 19;
+
+  return result_code;
+}
+const error_msg = (code: number) => {
+  let msg = "";
+  const ERROR: {[key: number]:string} = {
+    2: "undefined section id",
+    3: "duplicate section ids",
+    5: "corrupted parent hierarchy",
+    7: "invalid section",
+    11: "invalid period",
+    13: "invalid comma euler",
+    17: "invalid range",
+    19: "invalid limit"
+  };
+  const code_primes = factorize_int(code);
+  for (const p in code_primes){
+    msg += `\n${ERROR[p]}`;
+  }
+  return msg;
+}
+
+
 const private_data = new WeakMap();
 const private_rmaps = new WeakMap();
 export default class Canon {
   
-  constructor(schema: Optional<canon_schema>) {
+  constructor(schema?: any) {
+    if (schema != undefined && typeof schema == "object"){
+      for (const key in schema){
+        if (!(key in DEFAULT_SCHEMA)) delete schema[key];
+      }
+      for (const key in schema.params){
+        if (!(key in DEFAULT_SCHEMA.params)) delete schema.params[key];
+      }
+      schema.params = {...DEFAULT_SCHEMA.params, ...schema.params};
+    }
+
     const data = { ...DEFAULT_SCHEMA, ...schema,  } as canon_schema;
+    
+    const error_code = check_schema_validity(data);
+    if (error_code > 1) {
+      throw Error(error_msg(error_code)+"\n"+JSON.stringify(data));
+    }
+    
     private_data.set(this, data);
     if (data.params.limit) private_rmaps.set(this, new RatioMap(data.params.limit, data.params.range));
   }
