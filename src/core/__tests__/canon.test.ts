1a0ce36050551f093a5620bf039dacab48c1cfa0
Horolsky <horolsky89@outlook.com>
2021-04-05 14:07:33 +0300

    core: canon+db test

--- src/core/__tests__/canon.test.ts
+++ src/core/__tests__/canon.test.ts
@@ -2,6 +2,7 @@ import RatioMap from '../ratiomap';
 import Canon from '../canon'
 import { expect, test } from '@jest/globals';
 import { S_COMMA } from '../constants';
+import db from '../../db/firestore';
 
 test('creating Canon inst, correct: default values', ()=>{
     const x = new Canon();
@@ -13,4 +14,13 @@ test('creating Canon inst, correct: default values', ()=>{
 test('creating Canon inst, corrupted input: section id', ()=>{
     const x = () => {new Canon({sections: [{id: -1}]});}
     expect(x).toThrowError();
+})
+
+test('creating Canon from db doc', async ()=>{
+    const doc = await db.collection("schemata").doc('just').get()
+    if (doc.exists) {
+        const c = new Canon(doc.data());
+        expect(c.limit).toBe(5);
+    }
+    expect(doc.exists).toBe(true);
 })
