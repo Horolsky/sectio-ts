54fbd000e7d2f649c672f780be33251d373b29d7
Horolsky <horolsky89@outlook.com>
2021-04-05 13:57:43 +0300

    App module cleaned from db

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,7 @@
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+
 
 @Options({ 
   name: 'App',
@@ -22,19 +21,7 @@ import 'firebase/firestore';
   },
   methods: {
     initialize() {
-      //FIRESTORE
-      this.firebase = firebase;
-      firebase.initializeApp({
-        apiKey: "AIzaSyBCNrnag3orw2BTIn4hPR2i_n-5eEacr2w",
-        authDomain: "sectio-canonis.firebaseapp.com",
-        databaseURL: "https://sectio-canonis.firebaseio.com",
-        projectId: "sectio-canonis",
-        storageBucket: "sectio-canonis.appspot.com",
-        messagingSenderId: "674839962809",
-        appId: "1:674839962809:web:787b93e13ff8c63c2d6768",
-        measurementId: "G-MRG29FW0RK"
-      });
-      this.db = firebase.firestore();
+      //
     }
   }
 })
