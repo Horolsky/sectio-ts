96c928c718e54f81bda21f88f18e9b999a476d3b
Horolsky <horolsky89@outlook.com>
2021-04-06 19:24:01 +0300

    utils: exact bin search, put_to_sorted

--- src/utils/object.ts
+++ src/utils/object.ts
@@ -17,6 +17,27 @@ export function deep_merge(target: any, ...sources: any): undefined {
     }
     return deep_merge(target, ...sources);
 }
+/** insert element to sorted numeric or string array\
+ *  binary search for insertion index
+ *  return new index of inserted element */
+export function put_to_sorted<Type>(
+    target: Array<Type>, val: Type
+    ):number {
+    if (target.length == 0) {
+        target.push(val);
+        return 0;
+    }
+    if (typeof val != typeof target[0]) throw Error("incompactible types");
+    let start = -1;
+    let end = target.length;
+    let mid: number;
+    while (end - start > 1) {
+        mid = start + Math.trunc((end - start) / 2);
+        val > target[mid] ? start = mid : end = mid;
+    }
+    target.splice(end,0,val);
+    return end;
+}
 
 export default {
     obj_not_array,
--- src/utils/search.ts
+++ src/utils/search.ts
@@ -13,7 +13,7 @@ export const bin_search = (
     }
     return Math.abs(callback(val, arr[start])) < Math.abs(callback(val, arr[end])) ? start : end;
 }
-export const bin_search_float = (arr:Array<number>, val:number) => {
+export const bin_search_appr = (arr:Array<number>, val:number) => {
     let start = 0;
     let end = arr.length - 1;
     let mid: number;
@@ -23,7 +23,17 @@ export const bin_search_float = (arr:Array<number>, val:number) => {
     }
     return Math.abs(val - arr[start]) < Math.abs(val - arr[end]) ? start : end;
 }
+export const bin_search_exact = (arr:Array<number>, val:number) => {
+    let start = 0;
+    let end = arr.length-1;
+    let mid: number;
+    while (end - start > 1) {
+        mid = start + Math.trunc((end - start) / 2);
+        val > arr[mid] ? start = mid : end = mid;
+    }
+    return val === arr[start] ? start : -1;
+}
 export default {
-    bin_search_float,
+    bin_search_appr,
     bin_search
 }
