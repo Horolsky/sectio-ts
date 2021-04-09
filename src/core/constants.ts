92eebef9aebd6ba01c873e84fbe4b39fbf931ab0
Horolsky <horolsky89@outlook.com>
2021-04-09 19:47:25 +0300

    core: constants fix, added def comma range

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -3,9 +3,11 @@ export const PREC = 6e-13;
 //tested on difference of exact log2 values with toFixed(12)
 //min for comma division: 5e-14, min 64-bit: 3.552713678800501e-15
 /** log2 of syntonic comma (81/80) */
-export const S_COMMA = Math.log2(81) - Math.log2(80)
+export const S_COMMA = Math.log2(81/80);
 /** log2 of pythagorean comma (531441/524288) */
-export const P_COMMA = Math.log2(531441) - Math.log2(524288)
+export const P_COMMA = Math.log2(531441/524288);
+/** range for comma fraction approximation */
+export const COMMA_RANGE = 100;
 /** canonic approximation primes */
 export const APPROX_PRIMES = [2, 3, 5, 7, 11, 13, 17];
 /**
