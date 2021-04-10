29ed711120c7c173d15d7284479b778a00c8548a
Horolsky <horolsky89@outlook.com>
2021-04-10 21:27:23 +0300

    store: basic modules init

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { createVuexStore, State, Module } from 'vuex-simple';
+
+import {CanonData} from './modules/canon-data'
+import tabsData from './modules/tabs-data'
+import uiConfig from './modules/ui-config'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+export class RootModule {
+  @State()
+  public tab = 0;
+
+  @Module()
+  public canon = new CanonData(this);
+}
+export const STORE = new RootModule()
+export default createVuexStore(STORE, {
+  strict: false,
+  modules: {},
+  plugins: []
+});
+
+
